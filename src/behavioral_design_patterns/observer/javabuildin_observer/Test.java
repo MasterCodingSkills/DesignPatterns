04fdb50c6ecbdc1b8d941c23aecba40be41e1550
MasterCodingSkills <56904368+MasterCodingSkills@users.noreply.github.com>
2020-06-25 16:16:13 +0530

    java inbuild observer example start

---
+++ src/behavioral_design_patterns/observer/javabuildin_observer/Test.java
@@ -0,0 +1,7 @@
+package behavioral_design_patterns.observer.javabuildin_observer;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Observer Design patter");
+    }
+}
